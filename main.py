b5f61b49ededabd5f28e165fbd48bb26787c41bc
aelshi01 <56831995+aelshi01@users.noreply.github.com>
2022-03-22 17:58:42 +0000

    ml pipeline

--- main.py
+++ main.py
@@ -40,17 +40,42 @@ def go(config: DictConfig):
     if "preprocess" in steps_to_execute:
 
         ## YOUR CODE HERE: call the preprocess step
-        pass
+        _=mlflow.run(
+            os.path.join(root_path,"preprocess"),
+            "main",
+            parameters={
+                "input_artifact": "raw_data.parquet:latest",
+                "artifact_name": "preprocessed_data.csv"
+                "artifact_type": "preprocessed_data"
+                "artifact_description": "Data with preprocessing applied"
+            },
+        )
 
     if "check_data" in steps_to_execute:
 
         ## YOUR CODE HERE: call the check_data step
-        pass
-
+        _=mlflow.run(
+            os.path.join(root_path,"check_data"),
+            "main",
+            parameters={
+                "reference_artifact": config["main"]["reference_dataset"],
+                "sample_artifact": "preprocessed_data.csv:latest",
+                "ks_alpha": config["main"]["ks_alpha"]
+        
+            },
+        )
     if "segregate" in steps_to_execute:
-
+        _=mlflow.run(
+            os.path.join(root_path,"segregate"),
+            "main",
+            parameters={
         ## YOUR CODE HERE: call the segregate step
-        pass
+                "input_artifact": "preprocessed_data.csv:latest"
+                "artifact_root": "data",
+                "artifact_type": "segregated_data"
+                "test_size": config["main"]["test_size"]
+            },
+        )
 
     if "random_forest" in steps_to_execute:
 
@@ -61,12 +86,25 @@ def go(config: DictConfig):
             fp.write(OmegaConf.to_yaml(config["random_forest_pipeline"]))
 
         ## YOUR CODE HERE: call the random_forest step
-        pass
+        _=mlflow.run(
+            os.path.join(root_path,"segregate"),
+            "main",
+            parameters={
+                "train_data": "data_train.csv:latest",
+                "model_config": model_config
+            },
+        )
 
     if "evaluate" in steps_to_execute:
 
         ## YOUR CODE HERE: call the evaluate step
-        pass
+            os.path.join(root_path,"segregate"),
+            "main",
+            parameters={
+                "model_export": f"{config['random_forest_pipeline']['export_artifact']}:latest",
+                "test_data": "data_test.csv:latest"
+            },
+        )        
 
 
 if __name__ == "__main__":
